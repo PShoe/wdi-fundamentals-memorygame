18c79402df47658bc991c7154ffb93b7b8c68c1d
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-08-05 18:56:21 -0400

    created function checkForMatch

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,29 @@
-console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var checkForMatch = function(){
 
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-	 alert("You found a match!");}
-	 else { 
-	 alert("Sorry, try again.");}
-}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert ("You found a match!");
+  } else {
+      alert ("Sorry, try again.");
+  }
+
+};
+
+var flipCard = function (cardID){
+
+	console.log("User flipped " + cards[cardID]);
+	
+	cardsInPlay.push(cards[cardID]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+
+};
+
+flipCard(0);
+flipCard(2);
